a77ea5e55fc855cba5d6aa5dca9ea2b290fc9308
caiwenqing <qianyucha@foxmail.com>
2021-04-22 16:20:43 +0800

    去掉多余引用

--- app/src/main/java/com/zcrain/wanandroid/ui/qa/QAFragment.kt
+++ app/src/main/java/com/zcrain/wanandroid/ui/qa/QAFragment.kt
@@ -1,14 +1,12 @@
 package com.zcrain.wanandroid.ui.qa
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.zcrain.wanandroid.Constant
 import com.zcrain.wanandroid.R
